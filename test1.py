d7f7ef837a4a396f468bcacccb69ff3d95f50ba9
johnlee47 <33021726+johnlee47@users.noreply.github.com>
2018-07-01 02:14:59 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+
+a[] = {1,2,3,4}
+
+while True:
+    for i in a:
+            if i == 4:
+                tabs = len(a)
+            else:
+                tabs = 0
+            if tabs > 10:
+                a[3] = 0
+            elif tabs < 10:
+                a[3] == 7;
+            else:
